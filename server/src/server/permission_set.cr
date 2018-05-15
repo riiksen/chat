89cd2e41da68623f0d3b59867ee3f82e6f694d35
Mike <35075093+CerteXCm@users.noreply.github.com>
2018-05-16 00:01:56 +0200

    Added more things to end up later

--- server/src/server/permission_set.cr
+++ server/src/server/permission_set.cr
@@ -1,3 +1,24 @@
 struct PermissionSet
+  
+  @permissions : Array(Permission)
+  
+  def initialize(@permissions : Array(Permission))
+    
+  end
+  
+  def add_permission(permission : Permission)
+    
+  end  
+  
+  def del_permission(permission : Permission)
+  
+  end
 
+  def >=(that : PermissionSet)
+    
+  end
+end
+
+struct Permission
+  
 end
